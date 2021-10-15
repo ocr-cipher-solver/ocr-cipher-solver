27d25df235c0dd819009d68d19c995b5e83bf2b9
Ryan Singman <ryanjs@vt.edu>
2021-10-15 16:15:21 -0400

    made an integration test for OCR

--- tests/ocr_test.py
+++ tests/ocr_test.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from PIL import ImageDraw
 
 from ocr_cipher_solver.ocr import OCR
 
@@ -13,3 +14,16 @@ def test_ocr_with_covid_image():
 
     # run ocr on image
     charset = ocr.run(img)
+
+    # display bounding boxes on images
+    draw_img = ImageDraw.Draw(img)
+    for char in charset:
+        x0 = char.bounding_box.left
+        y0 = img.height - char.bounding_box.top
+        x1 = x0 + char.bounding_box.width
+        y1 = y0 + char.bounding_box.height
+
+        draw_img.rectangle((x0, y0, x1, y1))
+
+    # display image
+    img.show()
